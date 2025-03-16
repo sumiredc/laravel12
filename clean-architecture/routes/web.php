e10b8e1ea8247a2dcc0d4a9b476d12da518be5e3
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-03-16 19:01:04 +0900

    fix: setting

--- clean-architecture/routes/web.php
+++ clean-architecture/routes/web.php
@@ -5,8 +5,4 @@ declare(strict_types=1);
 use App\Http\Controllers\Error\NotFoundController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::fallback(NotFoundController::class);
--- clean-architecture/setpassport.sh
+++ clean-architecture/setpassport.sh
@@ -5,7 +5,14 @@ output=$(./vendor/bin/sail artisan passport:client --personal | tee /dev/tty)
 client_id=$(echo "$output" | grep 'Client ID' | awk '{print $NF}')
 client_secret=$(echo "$output" | grep 'Client secret' | awk '{print $NF}')
 
+if [ -z "$client_id" ] || [ -z "$client_secret" ]; then
+    echo "client_id: $client_id" >&2
+    echo "client_secret: $client_secret" >&2
+    echo "\n  ⚠️ Error: Failed to create personal client." >&2
+    exit 1
+fi
+
 sed -i '' -E "s|^PASSPORT_PERSONAL_ACCESS_CLIENT_ID=\"[^\"]*\"|PASSPORT_PERSONAL_ACCESS_CLIENT_ID=\"$client_id\"|" .env
 sed -i '' -E "s|^PASSPORT_PERSONAL_ACCESS_CLIENT_SECRET=\"[^\"]*\"|PASSPORT_PERSONAL_ACCESS_CLIENT_SECRET=\"$client_secret\"|" .env
 
-echo "\n  ✅️ Updated .env "
+echo "\n  ✅️ Updated .env " >&2
