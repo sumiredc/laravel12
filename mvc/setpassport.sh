5ef86e77457eb80b3114949e3b0e3ac26ecf4cdc
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-03-16 16:51:51 +0900

    ci: modify setpassport.sh

--- mvc/setpassport.sh
+++ mvc/setpassport.sh
@@ -5,6 +5,13 @@ output=$(./vendor/bin/sail artisan passport:client --personal | tee /dev/tty)
 client_id=$(echo "$output" | grep 'Client ID' | awk '{print $NF}')
 client_secret=$(echo "$output" | grep 'Client secret' | awk '{print $NF}')
 
+if [ -z "$client_id" ] || [ -z "$client_secret" ]; then
+    echo "client_id: $client_id"
+    echo "client_secret: $client_secret"
+    echo "\n  ⚠️ Error: Failed to create personal client."
+    exit 1
+fi
+
 sed -i '' -E "s|^PASSPORT_PERSONAL_ACCESS_CLIENT_ID=\"[^\"]*\"|PASSPORT_PERSONAL_ACCESS_CLIENT_ID=\"$client_id\"|" .env
 sed -i '' -E "s|^PASSPORT_PERSONAL_ACCESS_CLIENT_SECRET=\"[^\"]*\"|PASSPORT_PERSONAL_ACCESS_CLIENT_SECRET=\"$client_secret\"|" .env
 
