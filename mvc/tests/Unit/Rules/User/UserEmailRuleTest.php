0e27653ae603b259b4db73e38c36e4d2d627b742
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-03-16 18:27:37 +0900

    test: unit test no connection

--- mvc/tests/Unit/Rules/User/UserEmailRuleTest.php
+++ mvc/tests/Unit/Rules/User/UserEmailRuleTest.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 
 use App\Rules\User\UserEmailRule;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\PresenceVerifierInterface;
+
+\beforeEach(function () {
+    $this->presenceVerifierMock = Mockery::mock(PresenceVerifierInterface::class);
+    $this->presenceVerifierMock->shouldReceive('setConnection');
+    Validator::setPresenceVerifier($this->presenceVerifierMock);
+});
 
 \describe('validate', function () {
     \it('accepts valid email addresses', function ($v) {
+        $this->presenceVerifierMock->shouldReceive('getCount')->andReturn(0);
         $rule = new UserEmailRule;
         $validator = Validator::make(
             data: ['v' => $v],
@@ -33,4 +41,17 @@ use Illuminate\Support\Facades\Validator;
             'email with invalid character' => 'aaa#xxx.xxx',
             'email exceeding maximum length' => \sprintf('%s@xxxxx.xxx', \str_repeat('a', 91)),
         ]);
+
+    \it('rejects duplicate email addresses', function () {
+        $this->presenceVerifierMock->shouldReceive('getCount')->andReturn(1);
+
+        $email = 'aaa@xxx.xxx';
+        $rule = new UserEmailRule;
+        $validator = Validator::make(
+            data: ['v' => $email],
+            rules: ['v' => $rule]
+        );
+
+        \expect($validator->fails())->toBeTrue();
+    });
 });
